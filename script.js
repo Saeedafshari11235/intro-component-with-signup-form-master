e6f08d20c70dfda9631387eb169b7ef409c08335
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-12 20:57:47 +0430

    adding alerts

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let email = document.querySelector("#email")
 let password = document.querySelector("#password")
 //alerts
 let fnlrt = document.querySelector("#fn-alert")
+let lnlrt = document.querySelector("#ln-alert")
+let ealrt = document.querySelector("#ea-alert")
+let pwlrt = document.querySelector("#pw-alert")
 console.log(fnlrt)
 firstname.addEventListener("blur",()=>{
     if(firstname.value==""){
