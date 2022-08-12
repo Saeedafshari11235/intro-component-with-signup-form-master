c059086323f4cd3aad498f8cc195088499a42c28
Saeedafshari11235 <msafshari11235@gmail.com>
2022-08-12 20:56:43 +0430

    setting inputs

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+//inputs
 let firstname = document.querySelector("#firstname")
+let lastname = document.querySelector("#lastname")
+let email = document.querySelector("#email")
+let password = document.querySelector("#password")
+//alerts
 let fnlrt = document.querySelector("#fn-alert")
 console.log(fnlrt)
 firstname.addEventListener("blur",()=>{
     if(firstname.value==""){
-        fnlrt.setAttribute("display","block")
+        fnlrt.classList.remove("alertoff")
+        fnlrt.classList.add("alerton")
     }
 })
